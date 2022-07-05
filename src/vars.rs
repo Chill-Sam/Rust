7260886cbb4267c35f974e6ef4beb1bb9cb89c3b
Chill Sam <jonathan_wahrenberg@outlook.com>
2022-07-05 12:54:38 +0200

    Tested mutable variables

--- src/vars.rs
+++ src/vars.rs
@@ -1,5 +1,7 @@
 pub fn run() {
     let name = "Jonathan";
-    
-    println!("My name is {}", name);
+    let mut age = 37;
+
+    age = 38;
+    println!("My name is {} and I am {}", name, age);
 }
