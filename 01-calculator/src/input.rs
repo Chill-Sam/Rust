8ca2abeb7ea23a6f61713ca01fbdb10a98ba2cfc
Chill Sam <jonathan_wahrenberg@outlook.com>
2022-07-06 13:01:35 +0200

    Added comments

--- 01-calculator/src/input.rs
+++ 01-calculator/src/input.rs
@@ -1,17 +1,18 @@
 use std::io;
 
-
+// Returns f64 input
 pub fn numinput() -> f64
 {
-    let mut input = String::new(); io::stdin().read_line(&mut input).expect("Error, could not read from stdin.");
-    let float: f64 = input.trim().parse().expect("Not a valid number.");
+    let mut input = String::new(); io::stdin().read_line(&mut input).expect("Error, could not read from stdin."); // takes string input from stdin
+    let float: f64 = input.trim().parse().expect("Not a valid float."); // attemps to parse to f64. if failure, it is not a float
     return float;
 }
 
+// Takes input for an operation and returns a u16
 pub fn operationinput() -> u16
 {
-    let mut input = String::new(); io::stdin().read_line(&mut input).expect("Error, could not read from stdin.");
-    let operation: char = input.trim().parse().expect("Not a valid char.");
-    if operation == '+'{return 1;} if operation == '-'{return 2;} if operation == '*'{return 3;} if operation == '/'{return 4;}
-    else {println! ( "Not a operation." ); return 404;}
+    let mut input = String::new(); io::stdin().read_line(&mut input).expect("Error, could not read from stdin."); // takes string input from stdin
+    let operation: char = input.trim().parse().expect("Not a valid char."); // attemps to parse to char. if failute, it is not a char.
+    if operation == '+'{return 1;} if operation == '-'{return 2;} if operation == '*'{return 3;} if operation == '/'{return 4;} // If statements to return u16 depending on operation chosen.
+    else {println! ( "Not a operation." ); return 404;} // Returns 404 if input is not an operation
 }
