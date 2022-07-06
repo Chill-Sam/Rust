af10ed9f6de149fa767739b560d0c3f3e7c21446
Chill Sam <jonathan_wahrenberg@outlook.com>
2022-07-06 12:42:59 +0200

    Finished calculator.

--- 01-calculator/src/input.rs
+++ 01-calculator/src/input.rs
@@ -1,9 +1,17 @@
 use std::io;
 
 
-pub fn run() -> f64
+pub fn numinput() -> f64
 {
-    let mut input = String::new(); io::stdin().read_line(&mut input).expect("Not a valid string.");
-    let float: f64 = input.trim().parse().expect("Not a valid number");
+    let mut input = String::new(); io::stdin().read_line(&mut input).expect("Error, could not read from stdin.");
+    let float: f64 = input.trim().parse().expect("Not a valid number.");
     return float;
+}
+
+pub fn operationinput() -> u16
+{
+    let mut input = String::new(); io::stdin().read_line(&mut input).expect("Error, could not read from stdin.");
+    let operation: char = input.trim().parse().expect("Not a valid char.");
+    if operation == '+'{return 1;} if operation == '-'{return 2;} if operation == '*'{return 3;} if operation == '/'{return 4;}
+    else {println! ( "Not a operation." ); return 404;}
 }
--- 01-calculator/src/main.rs
+++ 01-calculator/src/main.rs
@@ -1,11 +1,16 @@
 mod input;
-mod add;
+mod operations;
 
 fn main() 
 {
-    println! ( "Pick first number: " ); let input1: f64 = input::run(); 
+    println! ( "Pick first number: " ); let input1: f64 = input::numinput(); 
+    println! ( "Pick second number: " ); let input2: f64 = input::numinput();
+    println! ( "Pick a operation. (+, -, *, /)" ); let operation: u16 = input::operationinput();
 
-    println! ( "Pick second number: " ); let input2: f64 = input::run();
-
-    let answer: f64 = add::add(input1, input2); println!( "{} + {} = {}", input1, input2, answer );
+    if operation == 1 {let answer: f64 = operations::add(input1, input2); println!( "{} + {} = {}", input1, input2, answer ); return;}
+    if operation == 2 {let answer: f64 = operations::sub(input1, input2); println!( "{} - {} = {}", input1, input2, answer ); return;}
+    if operation == 3 {let answer: f64 = operations::mul(input1, input2); println!( "{} * {} = {}", input1, input2, answer ); return;}
+    if operation == 4 {let answer: f64 = operations::div(input1, input2); println!( "{} / {} = {}", input1, input2, answer ); return;}
+    if operation == 404 {main();}
+    else {println!( "Error, something went wrong." ); main()}
 }
