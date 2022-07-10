f46ae2fc9edf8a8cae8a398b0bfe515646d06cec
Chill Sam <jonathan_wahrenberg@outlook.com>
2022-07-10 16:43:08 +0200

    Finished 06-topdown-triangle

--- 06-topdown-triangle/src/main.rs
+++ 06-topdown-triangle/src/main.rs
@@ -1,4 +1,23 @@
+use std::io;
+
 fn main() 
 {
-    println!("Hello, world!");
+    println! ( "Enter a number: " );
+
+    let mut input: String = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read from stdin");
+    
+    let mut max_num: u16 = input
+        .trim()
+        .parse()
+        .expect("Not a valid number (positive 16 bit integer)");
+    
+    while max_num > 0
+    {
+        let print = "*".repeat(max_num.into());
+        println! ("{}", print);
+        max_num = max_num - 1;
+    }
 }
