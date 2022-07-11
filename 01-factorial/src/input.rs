28818eb6bd9482120c33385d0b3c51aa695cc6ff
Chill Sam <jonathan_wahrenberg@outlook.com>
2022-07-11 18:55:29 +0200

    Removed unnecessary files

--- 01-factorial/src/input.rs
+++
@@ -1,8 +0,0 @@
-use std::io;
-
-pub fn intinput() -> i128
-{
-    let mut input = String::new(); io::stdin().read_line(&mut input).expect("Could not read from stdin.");
-    let num: i128 = input.trim().parse().expect("Not a valid number.");
-    return num;
-}
